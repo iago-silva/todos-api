5b4b66bc326919b139cf3d2cb3e9842d3371696a
Iago Silva <iago.silva@interage.in>
2019-03-08 00:45:54 -0300

    Remove comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,5 @@
-# app/models/item.rb
 class Item < ApplicationRecord
-  # model association
   belongs_to :todo
 
-  # validation
   validates_presence_of :name
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,8 +1,5 @@
-# app/models/todo.rb
 class Todo < ApplicationRecord
-  # model association
   has_many :items, dependent: :destroy
 
-  # validations
   validates_presence_of :title, :created_by
 end
