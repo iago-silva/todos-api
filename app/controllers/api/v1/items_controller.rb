e48ab74dd04ac84ff60a565b2e6b8537d9c68d2e
Iago Silva <iago.silva@interage.in>
2019-03-08 00:14:46 -0300

    Remove comments

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -2,29 +2,24 @@ class Api::V1::ItemsController < ApplicationController
   before_action :set_todo
   before_action :set_todo_item, only: [:show, :update, :destroy]
 
-  # GET /todos/:todo_id/items
   def index
     json_response(@todo.items)
   end
 
-  # GET /todos/:todo_id/items/:id
   def show
     json_response(@item)
   end
 
-  # POST /todos/:todo_id/items
   def create
     @todo.items.create!(item_params)
     json_response(@todo, :created)
   end
 
-  # PUT /todos/:todo_id/items/:id
   def update
     @item.update(item_params)
     head :no_content
   end
 
-  # DELETE /todos/:todo_id/items/:id
   def destroy
     @item.destroy
     head :no_content
